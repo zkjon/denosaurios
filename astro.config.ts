048a5fe5e8eb10f5b44cc71284ea6a5142f14a3e
zkjon <focusyts@gmail.com>
2026-03-17 13:04:01 +0100

    refactor(astro.config.ts): refactorizar el nombre del componente de las analíticas de vercel

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     starlight({
       components: {
-        Head: './src/components/Head.astro',
+        Head: './src/components/analiticas.astro',
       },
       locales: {
         root: {
