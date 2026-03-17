61c947db73b2122660ee49ff80cc9049d459a04e
zkjon <focusyts@gmail.com>
2026-03-17 12:33:56 +0100

    feat(analiticas): renombrar componente de analítica y actualizar la configuración

--- astro.config.ts
+++ astro.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
       },
 
       components: {
-        Head: '@/components/analytics.astro',
+        Head: '@/components/analiticas.astro',
       },
     }),
   ],
