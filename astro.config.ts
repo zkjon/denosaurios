f993b3dc6fea3c1f9499b96d7ecdc0236d18a547
zkjon <focusyts@gmail.com>
2026-03-20 09:10:25 +0100

    feat(docs): agregar archivos de documentación para el mesozoico aún vacíos

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     starlight({
       components: {
-        Head: './src/components/analiticas.astro',
+        Head: "./src/components/analiticas.astro",
       },
       locales: {
         root: {
@@ -36,25 +36,39 @@ export default defineConfig({
           label: "introducción",
           items: [{ label: "sobre mi", link: "/intro/sobre-mi/" }],
         },
-        { 
+        {
           label: "la tierra primitiva",
           items: [
             { label: "origen", link: "/tierra/origen/" },
             { label: "formación", link: "/tierra/formacion/" },
             { label: "primeros océanos", link: "/tierra/primeros-oceanos/" },
-            { label: "primeras moléculas", link: "/tierra/primeras-moleculas/" },
-            { label: "primeras formas de vida", link: "/tierra/primeras-formas-de-vida/" },
-            { label: "la gran oxidación", link: "/tierra/gran-oxidacion/" }
+            {
+              label: "primeras moléculas",
+              link: "/tierra/primeras-moleculas/",
+            },
+            {
+              label: "primeras formas de vida",
+              link: "/tierra/primeras-formas-de-vida/",
+            },
+            { label: "la gran oxidación", link: "/tierra/gran-oxidacion/" },
           ],
         },
-          {
+        {
           label: "la evolución de la vida",
           items: [
-            { label: "precámbrico", link: "/eras/precambriano/" },
+            { label: "precámbrico", link: "/eras/precambrico/" },
             { label: "paleozoico", link: "/eras/paleozoico/" },
-            
-          ]
-        }
+          ],
+        },
+        {
+          label: "mesozoico",
+          items: [
+            { label: "introducción", link: "/eras/mesozoico/" },
+            { label: "triásico", link: "/eras/mesozoico/triasico/" },
+            { label: "jurásico", link: "/eras/mesozoico/jurasico/" },
+            { label: "cretácico", link: "/eras/mesozoico/cretacico/" },
+          ],
+        },
       ],
       customCss: ["@/styles/global.css"],
       logo: {
